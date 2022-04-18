af7ec1d27ece59484b79bc2491d987c91930dd05
Pablo400 <alu0101311135@ull.edu.es>
2022-04-18 20:27:26 +0000

    Repo Operativo

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,13 @@
 "use strict";
-console.log("Hello World");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.add = void 0;
+const chalk_1 = __importDefault(require("chalk"));
+console.log(chalk_1.default.blue('Hola'));
+function add(num1, num2) {
+    return num1 + num2;
+}
+exports.add = add;
+add(5, 6);
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+import chalk from 'chalk';
+
+console.log(chalk.blue('Hola'));
+
 export function add(num1: number, num2: number) : number {
   return num1 + num2;
 }
